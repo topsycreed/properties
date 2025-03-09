7f0eb44cb55d60276e9ad0aaf592125dfd35f79b
gchursov <topsycreed@gmail.com>
2025-03-09 16:00:39 +0100

    Add properties files

--- src/test/java/configs/TestConfig.java
+++ src/test/java/configs/TestConfig.java
@@ -1,9 +1,35 @@
 package configs;
 
-import static constants.Constants.BASE_URL;
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TestConfig {
+    String env;
+    Properties properties;
+
+    public TestConfig() {
+        env = System.getProperty("env", "default");
+        properties = getPropertiesByEnv(env);
+    }
+
     public String getBaseUrl() {
-        return System.getProperty("baseUrl", BASE_URL);
+        String baseUrl = properties.getProperty("baseUrl");
+        assertNotNull(baseUrl, String.format("BaseUrl is not found in %s.properties", env));
+        System.out.println("Base URL: " + baseUrl);
+        return baseUrl;
+    }
+
+    private Properties getPropertiesByEnv(String env) {
+        Properties testProperties = new Properties();
+        try {
+            testProperties.load(getClass().getClassLoader().getResourceAsStream(env + ".properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("Cannot open %s.properties", env));
+        }
+        return testProperties;
     }
 }
