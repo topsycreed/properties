d101f1905b629938f39adc7efcf125282e8207b1
gchursov <topsycreed@gmail.com>
2025-03-09 14:59:34 +0100

    Add constant for base url

--- src/test/java/SeleniumTests.java
+++ src/test/java/SeleniumTests.java
@@ -1,16 +1,21 @@
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SeleniumTests {
     WebDriver driver;
+    private static final String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/";
 
     @BeforeEach
     void setup() {
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @AfterEach
@@ -20,9 +25,20 @@ class SeleniumTests {
 
     @Test
     void openHomePageTest() {
-        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        driver.get(BASE_URL);
 
-        Assertions.assertEquals("https://bonigarcia.dev/selenium-webdriver-java/", driver.getCurrentUrl());
-        Assertions.assertEquals("Hands-On Selenium WebDriver with Java", driver.getTitle());
+        assertEquals(BASE_URL, driver.getCurrentUrl());
+        assertEquals("Hands-On Selenium WebDriver with Java", driver.getTitle());
+    }
+
+    @Test
+    void openLoginPageTest() {
+        driver.get(BASE_URL);
+
+        driver.findElement(By.xpath("//a[@href = 'login-form.html']")).click();
+        WebElement title = driver.findElement(By.className("display-6"));
+
+        assertEquals(BASE_URL + "login-form.html", driver.getCurrentUrl());
+        assertEquals("Login form", title.getText());
     }
 }
